4f2c755352e9dec7034ce095140a8f10da5879fb
qui101672 <qui101672@gmail.com>
2013-12-30 00:25:30 +0700

    fix

---
+++ Administrator/Controls/DangNhapController.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+include_once($_SERVER['DOCUMENT_ROOT'] . '/PhpProject1/Models/Nguoidung.php');
+
+?>
---
+++ Administrator/Views/Dangnhap.php
@@ -0,0 +1,17 @@
+<form name="frmdangnhap" action="" method="post" >
+    <div class="dangnhap">
+        <h3>ĐĂNG NHẬP</h3>
+        <div class="">
+                    <span class="">Mã số:</span>
+                    <span class=""><input name="txtmaso" type="text" size="25"/></span>
+                    <br/>
+                    <span class="">Mật khẩu: </span>
+                    <span class=""><input name="txtmatkhau" type="password" size="25"/></span>
+                    <br/>
+          	<div class="" >
+          		<input name="" type="submit" value="Đăng nhập" onclick="" />
+        </div> 
+          		
+            </div>
+        </div>					
+</form>
